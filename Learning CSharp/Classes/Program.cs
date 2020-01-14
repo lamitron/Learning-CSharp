9b5cbc8e9247cd281ae54da7fb19dbff069898ab
lamitron <matthewadamsavin@gmail.com>
2020-01-14 19:53:49 +0000

    yes

---
+++ Learning CSharp/Classes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Matthew", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} intitial balance");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend pays me back");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
---
+++ Learning CSharp/Learning CSharp/Arrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Learning_CSharp
+{
+    class Arrays
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Matthew" };
+            string newname = "";
+            while (newname != "-1")
+            {
+                Console.WriteLine("What is your name? (-1 to stop)");
+                newname = Console.ReadLine();
+                if (newname != "-1")
+                {
+                    names.Add(newname);
+                }
+            }
+            foreach(var name in names)
+            {
+                Console.WriteLine($"Hello, {name}");
+            }
+        } 
+    }
+}
